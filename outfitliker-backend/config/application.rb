a1261b3890b46ecd908b0aa688df19c2bfd8340f
itslizkim <elizabeth.kim93@gmail.com>
2020-06-17 02:05:33 -0400

    bundle update

--- outfitliker-backend/config/application.rb
+++ outfitliker-backend/config/application.rb
@@ -24,6 +24,8 @@ module Outfitliker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.fallbacks = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
